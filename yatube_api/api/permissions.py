b32b9a0b46b2103d29f17677d789eade83538e52
Alexander Davydov <zk31ns@mail.ru>
2025-05-12 22:15:00 +0300

    ран версии

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,6 +2,7 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
+    """Привет"""
 
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
